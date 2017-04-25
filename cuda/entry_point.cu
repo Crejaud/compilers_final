3ca7dd33b7d27d4db6d22bece5dd3cad7fb349eb
Corentin Rejaud <crejaud@nbp-177-46.nbp.ruw.rutgers.edu>
2017-04-24 20:16:12 -0400

    remove try

--- cuda/entry_point.cu
+++ cuda/entry_point.cu
@@ -19,30 +19,27 @@ int main(int argc, char** argv) {
                         Block size: E.g., --bsize 512\n\
                         Block count: E.g., --bcount 192\n";
 
-  try {
-    int bsize = 0, bcount = 0;
-    int word_length = 1;
+  int bsize = 0, bcount = 0;
+  int word_length = 1;
+  // GET INPUT PARAMETERS
 
-    // GET INPUT PARAMETERS
-
-    for (int iii = 1; iii < argc; ++iii) {
-      if (!strcmp(argv[iii], "--word_length") && iii != argc - 1) {
-        word_length = atoi(argv[iii+1]);
-      }
-      else if (!strcmp(argv[iii], "--bsize") && iii != argc - 1) {
-        bsize = atoi(argv[iii+1]);
-      }
-      else if (!strcmp(argv[iii], "--bcount") && iii != argc - 1) {
-        bcount = atoi(argv[iii+1]);
-      }
+  for (int iii = 1; iii < argc; ++iii) {
+    if (!strcmp(argv[iii], "--word_length") && iii != argc - 1) {
+      word_length = atoi(argv[iii+1]);
     }
-
-    if (word_length <= 0 || bsize <= 0 || bcount <= 0) {
-      cerr << "Usage: " << usage;
-      return 1;
+    else if (!strcmp(argv[iii], "--bsize") && iii != argc - 1) {
+      bsize = atoi(argv[iii+1]);
     }
-
-    find_all_permutations(bsize, bcount, word_length, outputFile);
+    else if (!strcmp(argv[iii], "--bcount") && iii != argc - 1) {
+      bcount = atoi(argv[iii+1]);
+    }
   }
+
+  if (word_length <= 0 || bsize <= 0 || bcount <= 0) {
+    cerr << "Usage: " << usage;
+    return 1;
+  }
+
+  find_all_permutations(bsize, bcount, word_length, outputFile);
 
 }
